7f6ddad5a2e4898062f73540d4d574613ac25ce3
northernCold <meishiyu.hxd@gmail.com}>
2024-04-22 15:54:12 +0800

    feat: set default locale is en

--- lib/i18n.ts
+++ lib/i18n.ts
@@ -1,6 +1,4 @@
-import { useEffect, useState } from "react";
-
-export const locales = ["en-US", "en", "zh-CN", "zh"];
+export const locales = ["en", "zh"];
 
 export const localeNames: Record<string, string> = {
   en: "🇺🇸 English",
@@ -19,10 +17,6 @@ const dictionaries: Dictionaries = {
 };
 
 export const getDictionary = async (locale: string) => {
-  if (["zh-CN", "zh-TW", "zh-HK"].includes(locale)) {
-    return dictionaries["zh"]();
-  }
-
   if (dictionaries[locale]) {
     return dictionaries[locale]();
   }
--- middleware.ts
+++ middleware.ts
@@ -1,22 +1,6 @@
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
-import { match } from "@formatjs/intl-localematcher";
-import Negotiator from "negotiator";
-import { locales } from "./lib/i18n";
-
-// Get the preferred locale, similar to the above or using a library
-function getLocale(request: NextRequest) {
-  const languages = new Negotiator({
-    headers: {
-      "accept-language": request.headers.get("accept-language") as
-        | string
-        | undefined,
-    },
-  }).languages();
-  const defaultLocale = "en-US";
-
-  return match(languages, locales, defaultLocale);
-}
+import { locales, defaultLocale } from "./lib/i18n";
 
 export function middleware(request: NextRequest) {
   // Check if there is any supported locale in the pathname
@@ -27,7 +11,7 @@ export function middleware(request: NextRequest) {
   if (pathnameHasLocale) return;
 
   // Redirect if there is no locale
-  const locale = getLocale(request);
+  const locale = defaultLocale;
   request.nextUrl.pathname = `/${locale}${pathname}`;
   // e.g. incoming request is /products
   // The new URL is now /en-US/products
