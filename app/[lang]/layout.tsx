299728b60fffef0422ea5623f0dcada360a7031d
northernCold <meishiyu.hxd@gmail.com}>
2024-04-22 16:15:51 +0800

    fix: resolve website lang

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
-import GoogleAnalytics from "./GoogleAnalytics";
-import "./globals.css";
+import GoogleAnalytics from "../GoogleAnalytics";
+import "../globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,19 +10,19 @@ export const metadata: Metadata = {
   title: "Indie Road",
   description: "A free, self-training and mutual learning community",
   icons: {
-    icon: [
-      { url: '/favicon.svg', sizes: '48x48', type: 'image/svg+xml' },
-    ],
-  }
+    icon: [{ url: "/favicon.svg", sizes: "48x48", type: "image/svg+xml" }],
+  },
 };
 
 export default function RootLayout({
   children,
+  params,
 }: Readonly<{
   children: React.ReactNode;
+  params: { lang: string };
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang={params.lang} suppressHydrationWarning>
       <head />
       <body className={inter.className}>
         <ThemeProvider
